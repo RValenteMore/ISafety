d1e1bd171bf3697a6b10e687c5a0a46de84f70b8
RValenteMore <93331239+RValenteMore@users.noreply.github.com>
2021-11-26 12:27:25 +0000

    Alteracao do centro do mapa

--- frontEnd/src/Components/Mapa/Mapa.js
+++ frontEnd/src/Components/Mapa/Mapa.js
@@ -4,12 +4,12 @@ import './Mapa.css';
 
 function Mapa() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[39.6, -8]} zoom={13} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[41.803947324226186, -6.787917939108926]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
