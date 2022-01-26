418f00eec5b6bc7b35d73a3090e5ea13bec3864e
Ricardo Manuel Sineiro Valente <a37487@alunos.ipb.pt>
2022-01-26 10:32:28 +0000

    Update Loja

--- src/frontEnd/Components/Dashboard/Outlets/loja.js
+++ src/frontEnd/Components/Dashboard/Outlets/loja.js
@@ -1,27 +1,81 @@
-import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
-import api from '../../../Services/api';
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../../Services/api";
+import { Grid } from "@material-ui/core";
+
+import Peixaria from "../../../Assets/DashBoard/peixaria.png";
+import Talho from "../../../Assets/DashBoard/talho.png";
+import Fescos from "../../../Assets/DashBoard/frescos.png";
+import PadariaPastelaria from "../../../Assets/DashBoard/padaria_pastelaria.png";
 
 function Loja() {
-    const [loja, setLoja] = useState({});
-    const {id} = useParams();
-    api.get(`/loja/:${id}`).then((loja) => {
-        setLoja(loja.data)
-    })
-    return (
-        <div>
-            <h2>{loja.Nome}</h2>
-            <h3>Loja: {loja.CodigoLoja}</h3>
-            <p>
-            Distrito: {loja.Distrito}<br/>
-            Concelho: {loja.Concelho}<br/>
-            Morada: {loja.Morada}<br/>
-            Codigo Postal: {loja.CodigoPost}<br/>
-            Ano de Abertura: {loja.AnoAbertur}<br/>
-            Nivel de Risco: {loja.Nivel_risco}
-            </p>
-        </div>
-    )
+  const [loja, setLoja] = useState({});
+  const { id } = useParams();
+  api.get(`/loja/:${id}`).then((loja) => {
+    setLoja(loja.data);
+  });
+  return (
+    <div>
+      <h2>{loja.Nome}</h2>
+      <h3>Loja: {loja.CodigoLoja}</h3>
+      <p>
+        Distrito: {loja.Distrito}
+        <br />
+        Concelho: {loja.Concelho}
+        <br />
+        Morada: {loja.Morada}
+        <br />
+        Codigo Postal: {loja.CodigoPost}
+        <br />
+        Ano de Abertura: {loja.AnoAbertur}
+        <br />
+        Nivel de Risco: {loja.Nivel_risco}
+      </p>
+
+      {/**Secções coloridas*/}
+      <Grid container md={12} className="seccoes">
+        <Grid item className="peixaria">
+          <img className="imgCartoes" src={Peixaria} />
+          <h3 className="textoCartoes">Peixaria</h3>
+          <p className="textoCartoes">Nivel de risco nas últimas horas</p>
+          <h1 style={{ fontSize: 60, marginTop: 0 }} className="textoCartoes">
+            45%
+          </h1>
+        </Grid>
+        <Grid item className="talho">
+          <img className="imgCartoes" src={Talho} />
+          <h3 className="textoCartoes">Talho</h3>
+          <p className="textoCartoes">Nivel de risco nas últimas horas</p>
+          <h1 style={{ fontSize: 60, marginTop: 0 }} className="textoCartoes">
+            45%
+          </h1>
+        </Grid>
+        <Grid item className="frescos">
+          <img className="imgCartoes" src={Fescos} />
+          <h3 className="textoCartoes">Frescos</h3>
+          <p className="textoCartoes">Nivel de risco nas últimas horas</p>
+          <h1 style={{ fontSize: 60, marginTop: 0 }} className="textoCartoes">
+            45%
+          </h1>
+        </Grid>
+        <Grid item className="padariaPastelaria">
+          <img className="imgCartoes" src={PadariaPastelaria} />
+          <h3 className="textoCartoes" style={{ color: "white" }}>
+            Padaria e Pastelaria
+          </h3>
+          <p className="textoCartoes" style={{ color: "white" }}>
+            Nivel de risco nas últimas horas
+          </p>
+          <h1
+            style={{ fontSize: 60, color: "white", marginTop: 0 }}
+            className="textoCartoes"
+          >
+            45%
+          </h1>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default Loja;
--- src/frontEnd/Components/Dashboard/Outlets/todas.js
+++ src/frontEnd/Components/Dashboard/Outlets/todas.js
@@ -45,34 +45,6 @@ function Todas() {
           <Paper className={classes.TaxaIncidenciaGeral}></Paper>
           <Paper className={classes.NivelRiscoGeral}></Paper>
         </Grid>
-
-        {/**Secções coloridas*/}
-        <Grid container md={12} className="seccoes">
-            <Grid item className="peixaria">
-              <h3 className="textoCartoes">Peixaria</h3>
-              <p className="textoCartoes">Nivel de risco nas últimas horas</p>
-              <h1 style={{fontSize: 60}} className="textoCartoes">45%</h1>
-            </Grid>
-            <Grid item className="talho">
-              <h3 className="textoCartoes">Talho</h3>
-              <p className="textoCartoes">Nivel de risco nas últimas horas</p>
-              <h1 style={{fontSize: 60}} className="textoCartoes">45%</h1>
-            </Grid>
-            <Grid item className="frescos">
-              <h3 className="textoCartoes">Frescos</h3>
-              <p className="textoCartoes">Nivel de risco nas últimas horas</p>
-              <h1 style={{fontSize: 60}} className="textoCartoes">45%</h1>
-            </Grid>
-            <Grid item className="padariaPastelaria">
-              <h3 className="textoCartoes" style={{ color: "white" }}>
-                Padaria e Pastelaria
-              </h3>
-              <p className="textoCartoes" style={{ color: "white" }}>
-                Nivel de risco nas últimas horas
-              </p>
-              <h1 style={{fontSize: 60, color: "white"}} className="textoCartoes">45%</h1>
-            </Grid>
-        </Grid>
       </Grid>
     </div>
   );
