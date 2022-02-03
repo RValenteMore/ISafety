df0a3a9e04d1b5593f14c2d65cca63b45456dc85
Ricardo Manuel Sineiro Valente <a37487@alunos.ipb.pt>
2022-02-03 16:24:52 +0000

    cor do botao

--- src/frontEnd/Components/Dashboard/Outlets/definicoes.js
+++ src/frontEnd/Components/Dashboard/Outlets/definicoes.js
@@ -7,10 +7,10 @@ function Definicoes() {
         <div>
             <h1>Definicoes</h1>
             <button 
-                style={{background: "lightgreen", height: "30pt", widtht: "50pt"}}
+                style={{background: "#5EB6A0", height: "30pt", widtht: "50pt"}}
                 onClick={() => {navigate("/dashboard/definicoes/adduser")}}
             >
-                Novo Administrador
+                <h5 style={{margin: 0, color: "#335675"}}>Novo Administrador</h5>
             </button>
             <Outlet/>
         </div>
